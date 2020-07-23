66f7149c00e1b26a22354b0cc359d96c02469ba3
Crew Spence <csspence123@gmail.com>
2020-07-23 06:56:51 -0500

    Wrote working solution.

--- count_by_x_codewars.js
+++ count_by_x_codewars.js
@@ -11,6 +11,11 @@ countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
 countBy(2,5) === [2,4,6,8,10]
 */
 
-function countBy(x, n) {
+const countBy = (x, n) => {
+  let arr = [];
+  for(let i = 1; i < n + 1; i++) {
+    arr.push(x * i);
+  }
 
+  return arr;
 }
